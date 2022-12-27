043c702f70296e1f3a412f8af0842137a507e3b3
Alexander <spindler.alexander@outlook.com>
2022-12-27 11:53:06 +0100

    Update update.sh

--- server/update.sh
+++ server/update.sh
@@ -30,4 +30,5 @@ done
 
 download_file "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/target/ProtocolLib.jar" plugins/ProtocolLib.jar
 download_latest_github_release "BlueMap-Minecraft/BlueMap" "BlueMap-{VERSION}-spigot.jar" plugins/bluemap.jar
-download_file "https://dev.bukkit.org/projects/worldedit/files/latest" plugins/optional/worldedit.jar
+#download_file "https://dev.bukkit.org/projects/worldedit/files/latest" plugins/optional/worldedit.jar
+download_file "https://mediafilez.forgecdn.net/files/4162/203/worldedit-bukkit-7.2.13.jar" plugins/optional/worldedit.jar
